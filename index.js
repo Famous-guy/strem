17d6b2a6528582205a033f4787652cb843426038
Alphasec <95212748+alphasecio@users.noreply.github.com>
2023-02-01 09:19:15 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at https://localhost:${PORT}/`);
+});
